5cb4d5d230d0f5b076dff15e78d50f051c646889
JuliusMatadamas <julius3877@gmail.com>
2021-01-27 20:06:47 -0600

    se inicia con el formulario de login de la aplicacion.

---
+++ resources/js/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="fhw">
+        <div class="card shadow">
+            <div class="card-header text-center">
+                <h4><i class="fas fa-file-contract"></i> Intranet</h4>
+                <h6>Grupo Promociones</h6>
+            </div>
+
+            <div class="card-body">
+                <p>Ingresa tus datos para iniciar sesión en la aplicación.</p>
+
+                <form :action="url"></form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        props: {
+            url: {
+                type: String,
+                default: '/login'
+            }
+        },
+        mounted() {
+            console.log('Ok')
+        }
+    }
+</script>
+
+<style scoped>
+    .card {
+        animation: zoomIn;
+        animation-duration: 2s;
+    }
+    .fhw {
+        align-items: center;
+        border: solid 1px red;
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        width: 100%;
+    }
+</style>
--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,22 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, facere facilis nemo non odio placeat sequi. Ad amet culpa distinctio eligendi excepturi, impedit ipsum obcaecati provident, repudiandae similique voluptas, voluptate?</p>
+    <form action="{{ route('logout')  }}" method="post">
+        @csrf
+        <div class="row">
+            <div class="col-md-4">
+                <button type="submit" class="btn btn-block btn-dark">Log-out</button>
             </div>
         </div>
-    </div>
+    </form>
 </div>
 @endsection
